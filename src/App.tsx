21672a06f265572649ebea138262302751df71a4
shreya <shreyasingh222602@gmail.com>
2023-11-08 02:05:05 +0530

    modification in ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
 import React from "react";
 import { MdWifi } from "react-icons/md";
 import { BsBatteryFull } from "react-icons/bs";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { FaRegEdit } from "react-icons/fa";
 import { VscSend } from "react-icons/vsc";
+import { LuPaperclip } from "react-icons/lu";
 import chatData from "./data";
-
+import { BsCamera } from "react-icons/bs";
+import { VscDeviceCameraVideo } from "react-icons/vsc";
+import { GoChecklist } from "react-icons/go";
+import{BiSolidBadgeCheck} from "react-icons/bi"
+import { FaSignal } from "react-icons/fa";
 import "./App.css";
 
 function App() {
@@ -14,9 +18,10 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-row  justify-between">
-        <h1 className="text-[22px] font-semibold mx-4">9:41</h1>
-        <div className="flex  gap-2 ">
+        <h1 className="text-[22px] font-semibold mx-4 p-3 ">9:41</h1>
+        <div className="flex  gap-2 p-4 ">
           {/* <MdWifi size={"24px"} /> */}
+          <FaSignal size={"24px"} />
           <MdWifi size={"24px"} />
           <BsBatteryFull size={"24px"} />
         </div>
@@ -40,22 +45,22 @@ function App() {
           }
         />
         <div className="mx-4">
-          <p className="lg:text-2xl">
+          <p className="lg:text-[19px]">
             From{" "}
             <span className="font-bold text-[20px] lg:[28px] text-gray-700">
-             {chatMessages.from}
+              {chatMessages.from}
             </span>
           </p>
-          <p className="lg:text-2xl">
+          <p className="lg:text-[19px]">
             To{" "}
             <span className="font-bold text-[20px] lg:text-[26px] text-gray-700">
-             {chatMessages.to}
+              {chatMessages.to}
             </span>
           </p>
         </div>
       </div>
       <div
-        className="mt-4 h-screen flex m-auto "
+        className="mt-4 h-screen flex  "
         style={{
           opacity: "0.3",
           flex: 1,
@@ -66,18 +71,18 @@ function App() {
       ></div>
       <div className="flex mt-20">
         <div
-          className="mt-4 h-screen  "
+          className="mt-4 h-screen m-4  "
           style={{
-            opacity: "0.4",
+            opacity: "0.3",
             flex: 1,
             height: "1.5px",
             backgroundColor: "gray",
             width: "40%",
           }}
         ></div>
-        <p className="text-gray-300">7 November, 2023</p>
+        <p className="text-gray-400">7 November, 2023</p>
         <div
-          className="mt-4 h-screen  "
+          className="mt-4 h-screen m-4"
           style={{
             opacity: "0.4",
             flex: 1,
@@ -88,7 +93,7 @@ function App() {
         ></div>
       </div>
 
-      <div className=" flex  flex-col">
+      <div className=" flex  flex-col mx-6">
         {chatData.chats.map((chat) => {
           return (
             <>
@@ -97,13 +102,22 @@ function App() {
                 className={chat.sender.self ? "message self" : "message other"}
               >
                 {!chat.sender.self && (
-                  <img
-                    className="rounded-full h-[20%] w-[9%]  mt-2 lg:w-[53px]"
-                    src={chat.sender.image}
-                    alt="User"
-                  />
+                  // <div className="">
+                  <>
+                
+                    <img
+                      className="rounded-full h-[20%] w-[11%]  mt-2 lg:w-[53px] "
+                      src={chat.sender.image}
+                      alt="User"
+                    />
+                    {/* <div className="mt-[29px] my-20  ">
+                    <BiSolidBadgeCheck/>
+                    </div> */}
+                    
+                    </>
+                  // </div>
                 )}
-                <div className="bg-red-100 chat-section p-3 mt-4 lg:mt-9 mx-2">
+                <div className="bg-red-100 chat-section p-3 mt-4 lg:mt-9 mx-2 ">
                   <p>{chat.message}</p>
                 </div>
               </div>
@@ -112,40 +126,45 @@ function App() {
         })}
       </div>
 
-      <div className="flex justify-between mt-10 ">
+      <div className="flex justify-between  ">
         <div className="form-input relative mx-2 p-2">
-          <span className="absolute inset-y-0 right-4 pl-3 flex items-center">
-            <MdWifi size={"24px"} />
-          </span>
-          <span className="absolute inset-y-0 right-8 pr-3 flex items-center">
+          <span className="absolute inset-y-0 right-4 pl-3 flex items-center lg:mx-6 mx-1">
             <VscSend size={"24px"} />
           </span>
+
+          <div className="tooltip-container absolute inset-y-0 right-8 pr-3 flex items-center lg:mx-10">
+            <div className="tooltip">
+              <div className="tooltip rounded-full mt-2 mx-4">
+                {" "}
+                <LuPaperclip size={"23px"} />
+                <span className="tooltiptext rounded-full ">
+                  <div className="flex p-3 gap-5 text-center">
+                    <span className="">
+                      <BsCamera size={"25px"} />
+                    </span>
+                    <span className="">
+                      <VscDeviceCameraVideo size={"27px"} />
+                    </span>
+                    <span className="">
+                      <GoChecklist size={"25px"} />
+                    </span>
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+
           <input
-            className="pl-12 pr-12 py-3 w-full rounded-2xl"
-            type="password"
-            name="password"
+            className="pl-3  py-3  lg:w-full w-[100%] rounded-2xl "
+            type="email"
+            name="email"
             placeholder="Reply to @Rohit Yadav"
           />
         </div>
       </div>
-      {/* <div className="flex justify-between mt-10">
-  <div className="form-input relative">
-    <span className="relative">
-      <span className="tooltip">
-        <span className="tooltiptext t">This is the tooltip content</span>
-        <MdWifi size={"24px"} />
-      </span>
-    </span>
-    <input
-      className="pl-12 pr-12 py-3 w-full rounded-2xl"
-      type="password"
-      name="password"
-      placeholder="Reply to @Rohit Yadav"
-    />
-  </div>
-</div> */}
+
       <div
-        className="mt-12 h-screen flex m-auto rounded-full "
+        className="mt-4 h-screen flex my-6 m-auto rounded-full "
         style={{
           flex: 1,
           height: "6px",
@@ -153,9 +172,9 @@ function App() {
           width: "40%",
         }}
       ></div>
+      
     </div>
   );
 }
 
 export default App;
-
