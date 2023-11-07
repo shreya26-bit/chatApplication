3d70c1b4258d8bb89910d7d4dbff79abbde4b6c9
shreya <shreyasingh222602@gmail.com>
2023-11-07 19:27:24 +0530

    modification in file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,161 @@
-import React from 'react';
 
-import './App.css';
+import React from "react";
+import { MdWifi } from "react-icons/md";
+import { BsBatteryFull } from "react-icons/bs";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { FaRegEdit } from "react-icons/fa";
+import { VscSend } from "react-icons/vsc";
+import chatData from "./data";
+
+import "./App.css";
 
 function App() {
+  const chatMessages = chatData;
   return (
     <div className="App">
+      <div className="flex flex-row  justify-between">
+        <h1 className="text-[22px] font-semibold mx-4">9:41</h1>
+        <div className="flex  gap-2 ">
+          {/* <MdWifi size={"24px"} /> */}
+          <MdWifi size={"24px"} />
+          <BsBatteryFull size={"24px"} />
+        </div>
+      </div>
+      <div className="flex mt-8 justify-between">
+        <div className="flex gap-2 ">
+          <div className="mt-2">
+            <AiOutlineArrowLeft size={"23px"} />
+          </div>
+          <h1 className="text-[24px] font-bold">{chatMessages.name}</h1>
+        </div>
+        <div className="mx-2">
+          <FaRegEdit size={"22px"} />
+        </div>
+      </div>
+      <div className="flex flex-row mt-4">
+        <img
+          className="rounded-full h-[20%] w-[14%] lg:h-[10%] lg:w-[7%] mt-2 mx-2"
+          src={
+            "https://fastly.picsum.photos/id/551/160/160.jpg?hmac=DKAZaW3KPwMLhYwnJ-s_NOYKngMXo-nR1pEQzcNCgr0"
+          }
+        />
+        <div className="mx-4">
+          <p className="lg:text-2xl">
+            From{" "}
+            <span className="font-bold text-[20px] lg:[28px] text-gray-700">
+             {chatMessages.from}
+            </span>
+          </p>
+          <p className="lg:text-2xl">
+            To{" "}
+            <span className="font-bold text-[20px] lg:text-[26px] text-gray-700">
+             {chatMessages.to}
+            </span>
+          </p>
+        </div>
+      </div>
+      <div
+        className="mt-4 h-screen flex m-auto "
+        style={{
+          opacity: "0.3",
+          flex: 1,
+          height: "0.7px",
+          backgroundColor: "gray",
+          width: "100%",
+        }}
+      ></div>
+      <div className="flex mt-20">
+        <div
+          className="mt-4 h-screen  "
+          style={{
+            opacity: "0.4",
+            flex: 1,
+            height: "1.5px",
+            backgroundColor: "gray",
+            width: "40%",
+          }}
+        ></div>
+        <p className="text-gray-300">7 November, 2023</p>
+        <div
+          className="mt-4 h-screen  "
+          style={{
+            opacity: "0.4",
+            flex: 1,
+            height: "1.5px",
+            backgroundColor: "gray",
+            width: "40%",
+          }}
+        ></div>
+      </div>
 
+      <div className=" flex  flex-col">
+        {chatData.chats.map((chat) => {
+          return (
+            <>
+              <div
+                key={chat.id}
+                className={chat.sender.self ? "message self" : "message other"}
+              >
+                {!chat.sender.self && (
+                  <img
+                    className="rounded-full h-[20%] w-[9%]  mt-2 lg:w-[53px]"
+                    src={chat.sender.image}
+                    alt="User"
+                  />
+                )}
+                <div className="bg-red-100 chat-section p-3 mt-4 lg:mt-9 mx-2">
+                  <p>{chat.message}</p>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+
+      <div className="flex justify-between mt-10 ">
+        <div className="form-input relative mx-2 p-2">
+          <span className="absolute inset-y-0 right-4 pl-3 flex items-center">
+            <MdWifi size={"24px"} />
+          </span>
+          <span className="absolute inset-y-0 right-8 pr-3 flex items-center">
+            <VscSend size={"24px"} />
+          </span>
+          <input
+            className="pl-12 pr-12 py-3 w-full rounded-2xl"
+            type="password"
+            name="password"
+            placeholder="Reply to @Rohit Yadav"
+          />
+        </div>
+      </div>
+      {/* <div className="flex justify-between mt-10">
+  <div className="form-input relative">
+    <span className="relative">
+      <span className="tooltip">
+        <span className="tooltiptext t">This is the tooltip content</span>
+        <MdWifi size={"24px"} />
+      </span>
+    </span>
+    <input
+      className="pl-12 pr-12 py-3 w-full rounded-2xl"
+      type="password"
+      name="password"
+      placeholder="Reply to @Rohit Yadav"
+    />
+  </div>
+</div> */}
+      <div
+        className="mt-12 h-screen flex m-auto rounded-full "
+        style={{
+          flex: 1,
+          height: "6px",
+          backgroundColor: "black",
+          width: "40%",
+        }}
+      ></div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,121 @@
+const chatData = {
+ chats: [
+  {
+   "id": "5f8dcee49d9849b0aa720a024c319e9a",
+   "message": "Duis ac nulla varius diam ultrices rutrum.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam tempus scelerisque purus, sed mattis elit condimentum nec.Proin ipsum purus, laoreet quis dictum a, laoreet sed ligula.Etiam risus sapien, auctor eu volutpat sit amet, porta in nunc.",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/819/160/160.jpg?hmac=duWXAb-022KT3VnXfDCSyr0sLwddRYoP7RMFnidof_g",
+     "is_kyc_verified": false,
+     "self": false,
+     "user_id": "54fbcca87afa4527b28221df348018b1"
+   },
+   "time": "2023-11-07 00:05:53"
+ },
+ {
+   "id": "ca2eb132ead84f37b0103e104e41c140",
+   "message": "Nullam tempus scelerisque purus, sed mattis elit condimentum nec.Cras vel elit sed mi placerat pharetra eget vel odio.Integer ultricies malesuada quam.Aenean ullamcorper orci et vulputate fermentum.<br>Page 0<br>",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/551/160/160.jpg?hmac=DKAZaW3KPwMLhYwnJ-s_NOYKngMXo-nR1pEQzcNCgr0",
+     "is_kyc_verified": true,
+     "self": false,
+     "user_id": "73785ed67d034f6290b0334c6e756433"
+   },
+   "time": "2023-11-07 00:06:23"
+ },
+ {
+   "id": "f600569f1f554582a0cac8a0424a8fc4",
+   "message": "Nullam tempus scelerisque purus, sed mattis elit condimentum nec.Cras vel elit sed mi placerat pharetra eget vel odio.Aenean ullamcorper orci et vulputate fermentum.Etiam risus sapien, auctor eu volutpat sit amet, porta in nunc.<br>Page 0<br>",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/551/160/160.jpg?hmac=DKAZaW3KPwMLhYwnJ-s_NOYKngMXo-nR1pEQzcNCgr0",
+     "is_kyc_verified": true,
+     "self": false,
+     "user_id": "73785ed67d034f6290b0334c6e756433"
+   },
+   "time": "2023-11-07 00:06:53"
+ },
+ {
+   "id": "251810bde63c4202b1df49d657f31f98",
+   "message": "Duis ac nulla varius diam ultrices rutrum.Proin ipsum purus, laoreet quis dictum a, laoreet sed ligula.Cras erat dui, finibus vel lectus ac, pharetra dictum odio.Duis ac nulla varius diam ultrices rutrum.",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/819/160/160.jpg?hmac=duWXAb-022KT3VnXfDCSyr0sLwddRYoP7RMFnidof_g",
+     "is_kyc_verified": false,
+     "self": false,
+     "user_id": "54fbcca87afa4527b28221df348018b1"
+   },
+   "time": "2023-11-07 00:07:23"
+ },
+ {
+   "id": "8d2a90cd3b8348a3b3121fabe6b6bd46",
+   "message": "Integer ultricies malesuada quam.Nullam tempus scelerisque purus, sed mattis elit condimentum nec.",
+   "sender": {
+     "image":"https://fastly.picsum.photos/id/1072/160/160.jpg?hmac=IDpbpA5neYzFjtkdFmBDKXwgr-907ewXLa9lLk9JuA8",
+     "is_kyc_verified": true,
+     "self": true,
+     "user_id": "67eab7475e5e4dd0903e133705213b43"
+   },
+   "time": "2023-11-07 00:07:53"
+ },
+ {
+   "id": "9edb58ab25484b9e9e56fafeeec9e93e",
+   "message": "Cras erat dui, finibus vel lectus ac, pharetra dictum odio.Duis ac nulla varius diam ultrices rutrum.Proin ipsum purus, laoreet quis dictum a, laoreet sed ligula.",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/1072/160/160.jpg?hmac=IDpbpA5neYzFjtkdFmBDKXwgr-907ewXLa9lLk9JuA8",
+     "is_kyc_verified": true,
+     "self": true,
+     "user_id": "67eab7475e5e4dd0903e133705213b43"
+   },
+   "time": "2023-11-07 00:08:23"
+ },
+ {
+   "id": "2bb190f5289542cc98a9b0f3e4ece9e5",
+   "message": "Integer ultricies malesuada quam.Integer ultricies malesuada quam.",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/1072/160/160.jpg?hmac=IDpbpA5neYzFjtkdFmBDKXwgr-907ewXLa9lLk9JuA8",
+     "is_kyc_verified": true,
+     "self": true,
+     "user_id": "67eab7475e5e4dd0903e133705213b43"
+   },
+   "time": "2023-11-07 00:08:53"
+ },
+ {
+   "id": "27b35a0554554cb7995041a7431efcd2",
+   "message": "Aenean ullamcorper orci et vulputate fermentum.Cras erat dui, finibus vel lectus ac, pharetra dictum odio.Integer ultricies malesuada quam.Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.Integer ultricies malesuada quam.",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/551/160/160.jpg?hmac=DKAZaW3KPwMLhYwnJ-s_NOYKngMXo-nR1pEQzcNCgr0",
+     "is_kyc_verified": true,
+     "self": false,
+     "user_id": "73785ed67d034f6290b0334c6e756433"
+   },
+   "time": "2023-11-07 00:09:23"
+ },
+ {
+   "id": "8d3b86e041ff448f8761eac9484f67b2",
+   "message": "Aenean ullamcorper orci et vulputate fermentum.Quisque vitae varius ex, eu volutpat orci.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/648/160/160.jpg?hmac=AqrvRqv79fFWHWjjjm_Cn7QPPJ2JVox_CLRgzISsO4o",
+     "is_kyc_verified": false,
+     "self": false,
+     "user_id": "a6c04ceed74b447280f5fa7ab053adcc"
+   },
+   "time": "2023-11-07 00:09:53"
+ },
+ {
+   "id": "b7b84837e5014ec28d7c7720ea9bfae4",
+   "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Quisque vitae varius ex, eu volutpat orci.",
+   "sender": {
+     "image": "https://fastly.picsum.photos/id/819/160/160.jpg?hmac=duWXAb-022KT3VnXfDCSyr0sLwddRYoP7RMFnidof_g",
+     "is_kyc_verified": false,
+     "self": false,
+     "user_id": "54fbcca87afa4527b28221df348018b1"
+   },
+   "time": "2023-11-07 00:10:23"
+ }
+ ],
+ from: "Rajajinagar",
+ message: "Chat retrieved successfully",
+ name: "Trip No. 64",
+ status: "success",
+ to: "Indiranagar",
+};
+
+export default chatData;
